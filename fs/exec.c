2ccbd33f7170e3e40c78c4ced46abebc235d755c
physwizz <pollockg86@gmail.com>
2025-07-17 21:01:48 +1000

    fix error

--- fs/exec.c
+++ fs/exec.c
@@ -1846,7 +1846,7 @@ extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *ar
 			void *envp, int *flags);
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 				 void *argv, void *envp, int *flags);
-#endi
+#endif
 
 
 /*
